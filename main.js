ba0b414936d46ec7b06456fb3a81f69578a6203c
JPuge <jesper@puge.dk>
2021-05-02 22:16:46 +0200

    Add feature: Select routes by dates

--- main.js
+++ main.js
@@ -229,6 +229,37 @@ function removeFromSelectedRoutes(route) {
   updateRouteInfo(route);
 }
 
+function selectRoutesByDateRange(fromDate, toDate) {
+  var routesInDateRange = [];
+
+  for (var i = 0; i < routes.length; i++) {
+    var route = routes[i];
+    if ((fromDate == null || fromDate <= route.startTime) &&
+        (toDate == null || toDate >= route.endTime)) {
+      routesInDateRange.push(route);
+    }
+  }
+
+  setSelectedRoutes(routesInDateRange);
+}
+
+function selectRoutesByYMW(year, month, week) {
+  var routesInDateRange = [];
+
+  for (var i = 0; i < routes.length; i++) {
+    var route = routes[i];
+
+    if ((route.startTime != null) && 
+        (year == null || year == route.startTime.getFullYear()) &&
+        (month == null || month == route.startTime.getMonth()) &&
+        (week == null || week == route.startTime.getWeek())) {
+      routesInDateRange.push(route);
+    }
+  }
+
+  setSelectedRoutes(routesInDateRange)
+}
+
 function unhideAllRoutes() {
   if (!routesHidden) return;
   
@@ -831,4 +862,19 @@ async function addRouteToDb(route) {
     route.id = await getRouteId(route);
   };
 
+}
+
+/******** Utils ***********/
+// Source: https://weeknumber.com/how-to/javascript
+// Returns the ISO week of the date.
+Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+                        - 3 + (week1.getDay() + 6) % 7) / 7);
 }
